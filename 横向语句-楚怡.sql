5f18f54ff8bed054071d75b1b792e4d77a74749a
liuying1129 <liuying1129@163.com>
2018-07-04 15:03:34 +0800

    增加组合项目【外院送检项目】

--- 横向语句-楚怡.sql
+++ 横向语句-楚怡.sql
@@ -76,45 +76,12 @@ max(case when TempA1.combin_name='彩超' and TempA1.name='甲状腺' then TempA1.ite
 max(case when TempA1.combin_name='彩超' and TempA1.name='心脏' then TempA1.itemvalue end) as '彩超/心脏',
 max(case when TempA1.combin_name='彩超' and TempA1.name='血管' then TempA1.itemvalue end) as '彩超/血管',
 max(case when TempA1.combin_name='彩超' and TempA1.name='腔内' then TempA1.itemvalue end) as '彩超/腔内',
-max(case when TempA1.combin_name='彩超' and TempA1.name='检查者' then TempA1.itemvalue end) as '彩超/检查者'--,
-/*
-max(case when TempA1.combin_name='血常规' and TempA1.name='白细胞' then TempA1.itemvalue end) as '血常规/白细胞',
-max(case when TempA1.combin_name='血常规' and TempA1.name='红细胞' then TempA1.itemvalue end) as '血常规/红细胞',
-max(case when TempA1.combin_name='血常规' and TempA1.name='血红蛋白' then TempA1.itemvalue end) as '血常规/血红蛋白',
-max(case when TempA1.combin_name='血常规' and TempA1.name='红细胞比积' then TempA1.itemvalue end) as '血常规/红细胞比积',
-max(case when TempA1.combin_name='血常规' and TempA1.name='平均红细胞体积' then TempA1.itemvalue end) as '血常规/平均红细胞体积',
-max(case when TempA1.combin_name='血常规' and TempA1.name='平均血红蛋白含量' then TempA1.itemvalue end) as '血常规/平均血红蛋白含量',
-max(case when TempA1.combin_name='血常规' and TempA1.name='平均血红蛋白浓度' then TempA1.itemvalue end) as '血常规/平均血红蛋白浓度',
-max(case when TempA1.combin_name='血常规' and TempA1.name='血小板' then TempA1.itemvalue end) as '血常规/血小板',
-max(case when TempA1.combin_name='血常规' and TempA1.name='淋巴细胞百分比' then TempA1.itemvalue end) as '血常规/淋巴细胞百分比',
-max(case when TempA1.combin_name='血常规' and TempA1.name='单核细胞百分比' then TempA1.itemvalue end) as '血常规/单核细胞百分比',
-max(case when TempA1.combin_name='血常规' and TempA1.name='粒细胞百分比' then TempA1.itemvalue end) as '血常规/粒细胞百分比',
-max(case when TempA1.combin_name='血常规' and TempA1.name='淋巴细胞记数' then TempA1.itemvalue end) as '血常规/淋巴细胞记数',
-max(case when TempA1.combin_name='血常规' and TempA1.name='单核细胞记数' then TempA1.itemvalue end) as '血常规/单核细胞记数',
-max(case when TempA1.combin_name='血常规' and TempA1.name='粒细胞记数' then TempA1.itemvalue end) as '血常规/粒细胞记数',
-max(case when TempA1.combin_name='血常规' and TempA1.name='红细胞分布宽度标准差' then TempA1.itemvalue end) as '血常规/红细胞分布宽度标准差',
-max(case when TempA1.combin_name='血常规' and TempA1.name='血小板体积分布宽度' then TempA1.itemvalue end) as '血常规/血小板体积分布宽度',
-max(case when TempA1.combin_name='血常规' and TempA1.name='平均血小板体积' then TempA1.itemvalue end) as '血常规/平均血小板体积',
-max(case when TempA1.combin_name='血常规' and TempA1.name='血小板压积' then TempA1.itemvalue end) as '血常规/血小板压积',
-max(case when TempA1.combin_name='血常规' and TempA1.name='红细胞分布宽度变异系数' then TempA1.itemvalue end) as '血常规/红细胞分布宽度变异系数',
+max(case when TempA1.combin_name='彩超' and TempA1.name='检查者' then TempA1.itemvalue end) as '彩超/检查者',
 
-max(case when TempA1.combin_name='乙肝两对半' and TempA1.name='乙肝表面抗原' then TempA1.itemvalue end) as '乙肝两对半/乙肝表面抗原',
-max(case when TempA1.combin_name='乙肝两对半' and TempA1.name='乙肝表面抗体' then TempA1.itemvalue end) as '乙肝两对半/乙肝表面抗体',
-max(case when TempA1.combin_name='乙肝两对半' and TempA1.name='乙肝e抗原' then TempA1.itemvalue end) as '乙肝两对半/乙肝e抗原',
-max(case when TempA1.combin_name='乙肝两对半' and TempA1.name='乙肝e抗体' then TempA1.itemvalue end) as '乙肝两对半/乙肝e抗体',
-max(case when TempA1.combin_name='乙肝两对半' and TempA1.name='乙肝核心抗体' then TempA1.itemvalue end) as '乙肝两对半/乙肝核心抗体',
+max(case when TempA1.combin_name='外院送检项目' and TempA1.name='电子胃镜' then TempA1.itemvalue end) as '外送/电子胃镜',
+max(case when TempA1.combin_name='外院送检项目' and TempA1.name='电子肠镜' then TempA1.itemvalue end) as '外送/电子肠镜',
+max(case when TempA1.combin_name='外院送检项目' and TempA1.name='经颅多普勒' then TempA1.itemvalue end) as '外送/经颅多普勒'
 
-max(case when TempA1.combin_name='尿常规' and TempA1.name='潜  血' then TempA1.itemvalue end) as '尿常规/潜  血',
-max(case when TempA1.combin_name='尿常规' and TempA1.name='尿胆原' then TempA1.itemvalue end) as '尿常规/尿胆原',
-max(case when TempA1.combin_name='尿常规' and TempA1.name='亚硝酸盐' then TempA1.itemvalue end) as '尿常规/亚硝酸盐',
-max(case when TempA1.combin_name='尿常规' and TempA1.name='蛋白质' then TempA1.itemvalue end) as '尿常规/蛋白质',
-max(case when TempA1.combin_name='尿常规' and TempA1.name='酮  体' then TempA1.itemvalue end) as '尿常规/酮  体',
-max(case when TempA1.combin_name='尿常规' and TempA1.name='比  重' then TempA1.itemvalue end) as '尿常规/比  重',
-max(case when TempA1.combin_name='尿常规' and TempA1.name='ＰＨ值' then TempA1.itemvalue end) as '尿常规/ＰＨ值',
-max(case when TempA1.combin_name='尿常规' and TempA1.name='胆红素' then TempA1.itemvalue end) as '尿常规/胆红素',
-max(case when TempA1.combin_name='尿常规' and TempA1.name='葡萄糖' then TempA1.itemvalue end) as '尿常规/葡萄糖',
-max(case when TempA1.combin_name='尿常规' and TempA1.name='白细胞' then TempA1.itemvalue end) as '尿常规/白细胞'
-*/
  from 
 (
 select cc.unid,cc.caseno, cc.patientname,cc.sex,cc.age,
@@ -125,10 +92,10 @@ left join
 select cc2.patientname,cc2.sex,cc2.age,cv2.combin_name ,cv2.Name,cv2.english_name,cv2.itemvalue,cv2.Unit,cv2.Min_value,cv2.Max_value 
 from view_chk_con_all cc2,view_chk_valu_all cv2 where cc2.unid=cv2.pkunid
 and isnull(cv2.itemvalue,'')<>'' and cv2.issure=1
-and cc2.check_date>getdate()-60--控制时间范围内的检验结果
+and cc2.check_date>getdate()-260--控制时间范围内的检验结果
 ) TempA2 on TempA2.patientname=cc.patientname and isnull(TempA2.sex,'')=isnull(cc.sex,'') and replace(isnull(TempA2.age,''),'岁','')=replace(isnull(cc.age,''),'岁','')
-where cc.combin_id like '%体检2组%'--控制工作组别的人员基本信息
-and cc.check_date>getdate()-60--控制时间范围内的人员基本信息
+where cc.combin_id like '%三公司体检6组%'--控制工作组别的人员基本信息
+and cc.check_date>getdate()-260--控制时间范围内的人员基本信息
 and isnull(cc.patientname,'')<>''
 ) TempA1
 
