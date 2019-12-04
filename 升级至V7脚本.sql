16f68205f9cf1167d44dbf0072bbe33e278f4d9b
liuying <liuying@baibu.la>
2019-12-04 21:54:56 +0800

    更改系统代码-Schedule WebSocket服务地址

--- 升级至V7脚本.sql
+++ 升级至V7脚本.sql
@@ -4125,8 +4125,8 @@ if not exists (select 1 from CommCode where TypeName='系统代码' and Remark='
 GO
 
 --20190130 WebSocket服务端-新结果提醒
-if not exists (select 1 from CommCode where TypeName='系统代码' and Remark='WebSocket服务端' and Reserve='新结果提醒')
-  insert into CommCode (typename,id,name,remark,Reserve,sysname) values ('系统代码','0008','ws://localhost:8081','WebSocket服务端','新结果提醒','LIS')
+if not exists (select 1 from CommCode where TypeName='系统代码' and Remark='Schedule WebSocket服务地址')
+  insert into CommCode (typename,id,name,remark,sysname) values ('系统代码','0008','ws://localhost:8081','Schedule WebSocket服务地址','LIS')
 GO
 
 if not exists(select * from CommCode where TypeName='工具菜单' and name='报表编辑器')
