2860ef3236641803c06b751a48f5cb3144bbe88b
liuying1129 <liuying1129@163.com>
2023-10-20 09:31:02 +0800

    GA已停止服务，删除softMeter调用代码中对GA的支持

--- softMeter_globalVar.pas
+++ softMeter_globalVar.pas
@@ -28,7 +28,8 @@
 ///
 ///  约束条件：
 ///  1、因本单元调用了LYFunction.dll(动态调用函数GetVersionLY),
-///     故应用程序目录必须存在该DLL                       
+///     故应用程序目录必须存在该DLL
+///  2、softMeter 2.0不支持WinXP
 ///
 //////////////////////////////////////////////////////////////
 
@@ -52,6 +53,9 @@ TYPE
   TDLL_GetVersionLY=function(const AFileName:Pchar):Pchar;stdcall;
 
 const
+  //Google Analytics property ID
+  GooglePropertyID = 'G-9EXL8X8N5M';//'UA-207373569-1';
+  
   ApiSecret = 'J_bc0WaGShOHUpUCxwweCA';
 
   //GA的Source值:应用名称+AppVersion+AppEdition                    
@@ -77,17 +81,7 @@ var
   H_LYFunction_LIB:THANDLE;
   DLL_GetVersionLY:TDLL_GetVersionLY;
 
-  osVerInfo:TOSVersionInfo;
-  GooglePropertyID:PChar;//Google Analytics property ID
-
 initialization
-  //softMeter 2.0支持GA4,但不支持WinXP
-  //softMeter 1.4.3及1.4.4支持WinXP,但不支持GA4
-  //所以,如操作系统为XP,手动替换为softMeter V1.4.3或V1.4.4,至少可在GA中查看埋点数据
-  GooglePropertyID:='G-9EXL8X8N5M';
-  osVerInfo.dwOSVersionInfoSize:=SizeOf(TOSVersionInfo);
-  if GetVersionEx(osVerInfo) and(osVerInfo.dwMajorVersion=5) then GooglePropertyID:='UA-207373569-1';
-
   //动态加载LYFunction.dll,并执行函数GetVersionLY begin
   H_LYFunction_LIB:=LOADLIBRARY('LYFunction.dll');
   IF H_LYFunction_LIB=0 THEN BEGIN raise Exception.Create('动态链接库LYFunction.dll不存在!');EXIT; END;
