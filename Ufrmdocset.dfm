a895ed2b2c8a16650cd17b15d18a92afd94c10be
liuying1129 <liuying1129@163.com>
2025-09-23 16:25:27 +0800

    优化【人员设置】的操作体验

--- Ufrmdocset.dfm
+++ Ufrmdocset.dfm
@@ -1,9 +1,10 @@
 object frmdocset: Tfrmdocset
   Left = 202
   Top = 131
-  Width = 544
-  Height = 375
+  BorderStyle = bsDialog
   Caption = #20154#21592#35774#32622
+  ClientHeight = 336
+  ClientWidth = 528
   Color = clSkyBlue
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -20,9 +21,9 @@ object frmdocset: Tfrmdocset
   TextHeight = 13
   object DBGrid1: TDBGrid
     Left = 0
-    Top = 98
+    Top = 108
     Width = 528
-    Height = 238
+    Height = 228
     Align = alClient
     Color = 16767438
     DataSource = DataSourcedoclist
@@ -33,53 +34,39 @@ object frmdocset: Tfrmdocset
     TitleFont.Height = -13
     TitleFont.Name = #23435#20307
     TitleFont.Style = []
-    OnCellClick = DBGrid1CellClick
   end
   object Panel2: TPanel
     Left = 0
-    Top = 41
+    Top = 76
     Width = 528
-    Height = 57
+    Height = 32
     Align = alTop
     Color = clSkyBlue
     TabOrder = 1
-    object Label1: TLabel
-      Left = 8
-      Top = 32
-      Width = 138
-      Height = 13
-      Caption = #27880#65306#24102'[*]'#30340#24517#39035#36755#20837#12290
-      Font.Charset = DEFAULT_CHARSET
-      Font.Color = clRed
-      Font.Height = -13
-      Font.Name = #23435#20307
-      Font.Style = []
-      ParentFont = False
-    end
     object Label2: TLabel
-      Left = 356
-      Top = 7
+      Left = 350
+      Top = 10
       Width = 104
       Height = 13
       Caption = #25805#20316#25152#26377#31185#23460#39033#30446
     end
     object LabeledEdit1: TLabeledEdit
-      Left = 80
-      Top = 4
+      Left = 74
+      Top = 7
       Width = 85
       Height = 19
       Color = 16767438
       Ctl3D = False
-      EditLabel.Width = 73
+      EditLabel.Width = 52
       EditLabel.Height = 13
-      EditLabel.Caption = #29992#25143#20195#30721'[*]'
+      EditLabel.Caption = #29992#25143#20195#30721
       LabelPosition = lpLeft
       ParentCtl3D = False
       TabOrder = 0
     end
     object LabeledEdit2: TLabeledEdit
-      Left = 247
-      Top = 4
+      Left = 241
+      Top = 7
       Width = 85
       Height = 19
       Color = 16767438
@@ -93,8 +80,8 @@ object frmdocset: Tfrmdocset
       TabOrder = 1
     end
     object ComboBox1: TComboBox
-      Left = 462
-      Top = 3
+      Left = 456
+      Top = 6
       Width = 40
       Height = 21
       ItemHeight = 13
@@ -106,116 +93,92 @@ object frmdocset: Tfrmdocset
   end
   object Panel1: TPanel
     Left = 0
-    Top = 0
+    Top = 36
     Width = 528
-    Height = 41
+    Height = 40
     Align = alTop
     Color = clSkyBlue
     TabOrder = 2
     object suiButton1: TSpeedButton
       Left = 5
-      Top = 8
+      Top = 7
       Width = 58
       Height = 27
-      Caption = #26032#22686'F2'
+      Caption = #26032#22686
       Transparent = False
       OnClick = suiButton1Click
     end
     object suiButton2: TSpeedButton
       Left = 69
-      Top = 8
+      Top = 7
       Width = 58
       Height = 27
-      Caption = #20445#23384'F3'
+      Caption = #20445#23384
       Transparent = False
       OnClick = suiButton2Click
     end
     object suiButton3: TSpeedButton
       Left = 133
-      Top = 8
+      Top = 7
       Width = 58
       Height = 27
-      Caption = #21024#38500'F4'
+      Caption = #21024#38500
       Transparent = False
       OnClick = suiButton3Click
     end
-    object DBLookupComboBox1: TDBLookupComboBox
-      Left = 354
-      Top = 12
-      Width = 145
-      Height = 19
-      Color = 16767438
-      Ctl3D = False
-      DropDownRows = 20
-      ListSource = DataSourcedeptlist
-      ParentCtl3D = False
-      TabOrder = 0
-      OnClick = DBLookupComboBox1Click
-    end
     object BitBtn1: TBitBtn
-      Left = 200
-      Top = 8
+      Left = 382
+      Top = 7
       Width = 113
       Height = 27
       Caption = #23548#20986#37096#38376#20154#21592
-      TabOrder = 1
+      TabOrder = 0
       OnClick = BitBtn1Click
     end
   end
-  object DataSourcedeptlist: TDataSource
-    DataSet = ADOdeptlist
-    Left = 492
-    Top = 16
-  end
-  object ADOdeptlist: TADOQuery
-    Parameters = <>
-    SQL.Strings = (
-      'select id as '#31185#23460#20195#30721','
-      
-        'name as '#31185#23460#21517#31216',unid as '#21807#19968#32534#21495' from CommCode where TypeName='#39#37096#38376#39' orde' +
-        'r by id'
-      '')
-    Left = 456
-    Top = 16
+  object Panel3: TPanel
+    Left = 0
+    Top = 0
+    Width = 528
+    Height = 36
+    Align = alTop
+    Color = clSkyBlue
+    TabOrder = 3
+    object Label1: TLabel
+      Left = 18
+      Top = 11
+      Width = 26
+      Height = 13
+      Caption = #37096#38376
+    end
+    object ComboBox2: TComboBox
+      Left = 47
+      Top = 7
+      Width = 145
+      Height = 21
+      Ctl3D = True
+      DropDownCount = 20
+      ItemHeight = 13
+      ParentCtl3D = False
+      TabOrder = 0
+      OnChange = ComboBox2Change
+      OnDropDown = ComboBox2DropDown
+    end
   end
   object ADOdoclist: TADOQuery
     AfterScroll = ADOdoclistAfterScroll
     Parameters = <>
-    Left = 444
-    Top = 248
+    Left = 44
+    Top = 136
   end
   object DataSourcedoclist: TDataSource
     DataSet = ADOdoclist
-    Left = 476
-    Top = 248
-  end
-  object ActionList1: TActionList
-    Left = 292
-    Top = 16
-    object Action1: TAction
-      Caption = 'Action1'
-      ShortCut = 113
-    end
-    object Action2: TAction
-      Caption = 'Action2'
-      ShortCut = 114
-    end
-    object Action3: TAction
-      Caption = 'Action3'
-      ShortCut = 115
-    end
-    object Action4: TAction
-      Caption = 'Action4'
-      ShortCut = 116
-    end
-    object Action5: TAction
-      Caption = 'Action5'
-      ShortCut = 117
-    end
+    Left = 76
+    Top = 136
   end
   object DosMove1: TDosMove
     Active = True
-    Left = 256
+    Left = 320
     Top = 16
   end
   object LYDataToExcel1: TLYDataToExcel
--- Ufrmdocset.pas
+++ Ufrmdocset.pas
@@ -3,70 +3,58 @@ unit Ufrmdocset;
 interface
 
 uses
-  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, ComCtrls, StdCtrls,  ExtCtrls, 
-  Grids, DBGrids,  DBCtrls, DB, ADODB,
-  ActnList, DosMove, Buttons, ULYDataToExcel;
+  Windows, Messages, SysUtils, Classes, Controls, Forms,
+  Dialogs, StdCtrls, Grids, DBGrids, DB, ADODB,
+  DosMove, Buttons, ULYDataToExcel, StrUtils, ExtCtrls;
 
 type
   Tfrmdocset = class(TForm)
-    DataSourcedeptlist: TDataSource;
-    ADOdeptlist: TADOQuery;
     ADOdoclist: TADOQuery;
     DataSourcedoclist: TDataSource;
-    ActionList1: TActionList;
-    Action1: TAction;
-    Action2: TAction;
-    Action3: TAction;
-    Action4: TAction;
-    Action5: TAction;
     DosMove1: TDosMove;
     LYDataToExcel1: TLYDataToExcel;
     DBGrid1: TDBGrid;
     Panel2: TPanel;
-    Label1: TLabel;
     LabeledEdit1: TLabeledEdit;
     LabeledEdit2: TLabeledEdit;
     Panel1: TPanel;
     suiButton1: TSpeedButton;
     suiButton2: TSpeedButton;
     suiButton3: TSpeedButton;
-    DBLookupComboBox1: TDBLookupComboBox;
     BitBtn1: TBitBtn;
     Label2: TLabel;
     ComboBox1: TComboBox;
-    procedure DBLookupComboBox1Click(Sender: TObject);
+    Panel3: TPanel;
+    Label1: TLabel;
+    ComboBox2: TComboBox;
     procedure suiButton1Click(Sender: TObject);
     procedure suiButton2Click(Sender: TObject);
-    procedure DBGrid1CellClick(Column: TColumn);
     procedure suiButton3Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
     procedure FormClose(Sender: TObject; var Action: TCloseAction);
     procedure ADOdoclistAfterScroll(DataSet: TDataSet);
-    procedure suiButton2KeyDown(Sender: TObject; var Key: Word;
-      Shift: TShiftState);
     procedure FormShow(Sender: TObject);
     procedure BitBtn1Click(Sender: TObject);
-
+    procedure ComboBox2DropDown(Sender: TObject);
+    procedure ComboBox2Change(Sender: TObject);
   private
+    function GetDepUnid:string;
+    procedure UpdateADOdoclist;
     procedure docrefresh;
     { Private declarations }
   public
     { Public declarations }
   end;
 
-function frmdocset: Tfrmdocset;
 //var
+function frmdocset: Tfrmdocset;
 
 implementation
 uses  UDM, SDIMAIN;
-const
-  sqlstr=' SELECT  id AS 用户代码, name AS 用户名称, pinyin AS 英文名,ShowAllTj as 所有科室项目,unid AS 唯一编号'+
-         ' FROM worker where pkdeptid=:P_pkdeptid order by id';
+
 var
   ifdocnewadd:boolean;
   ffrmdocset: Tfrmdocset;
-
 
 {$R *.dfm}
 
@@ -76,39 +64,52 @@ begin
   result:=ffrmdocset;
 end;
 
-procedure Tfrmdocset.DBLookupComboBox1Click(Sender: TObject);
+procedure Tfrmdocset.FormClose(Sender: TObject; var Action: TCloseAction);
 begin
-  if (not ADOdeptlist.Active) or(ADOdeptlist.RecordCount=0) then exit;
-
-    ADOdoclist.Close;
-    ADOdoclist.Parameters.ParamByName('P_pkdeptid').Value:=ADOdeptlist.fieldbyname('唯一编号').AsInteger;
-    ADOdoclist.Open;
-    docrefresh;
-  
+  action:=cafree;
+  if ffrmdocset=self then ffrmdocset:=nil;
 end;
 
 procedure Tfrmdocset.suiButton1Click(Sender: TObject);
 begin
-      if not ADOdoclist.Active then exit;
-      LabeledEdit1.Clear;
-      LabeledEdit2.Clear;
-      LabeledEdit1.SetFocus;
-      ADOdoclist.Edit;
-      ADOdoclist.Append;
-      ifdocnewadd:=true;
+  if not ADOdoclist.Active then exit;
+  
+  if trim(GetDepUnid)='' then//用户体验更好
+  begin
+    MessageDlg('新增前请先选择部门！',mtError,[MBOK],0);
+    exit;
+  end;
+
+  LabeledEdit1.Clear;
+  LabeledEdit2.Clear;
+  LabeledEdit1.SetFocus;
+  
+  ifdocnewadd:=true;
 end;
 
 procedure Tfrmdocset.suiButton2Click(Sender: TObject);
 Var
    adotemp11,adotemp12: tadoquery;
    iid:string;
+   dep_unid:string;
 Begin
    if ifdocnewadd then
    begin
         ifdocnewadd:=false;
+
+        dep_unid:=GetDepUnid;
+
+        if trim(dep_unid)='' then
+        begin
+          MessageDlg('请选择部门！',mtError,[MBOK],0);
+          exit;
+        end;
         
-        if (trim(LabeledEdit1.Text)='') {or (trim(LabeledEdit2.Text)='')} then
-              begin showmessage('用户代码不能为空！'); exit; end;
+        if trim(LabeledEdit1.Text)='' then
+        begin
+          MessageDlg('用户代码不能为空！',mtError,[MBOK],0);
+          exit;
+        end;
 
         adotemp12:=tadoquery.Create(nil);
         adotemp12.Connection:=DM.ADOConnection1;
@@ -116,24 +117,30 @@ Begin
         adotemp12.SQL.Clear;
         adotemp12.SQL.Text:='select * from worker ';
         adotemp12.Open;
-        if (adotemp12.Locate('id',trim(LabeledEdit1.text),[loCaseInsensitive]) {or adotemp12.Locate('name',trim(LabeledEdit2.text),[loCaseInsensitive])})
-            then begin showmessage('已存在该用户代码/用户名称，请更换！'); adotemp12.Free; exit; end;
+        if adotemp12.Locate('id',trim(LabeledEdit1.text),[loCaseInsensitive]) then
+        begin
+          MessageDlg('已存在该用户代码，请更换！',mtError,[MBOK],0);
+          adotemp12.Free;
+          exit;
+        end;
+        
         adotemp12.Close;
         adotemp12.SQL.Clear;
         adotemp12.SQL.Text:='Insert into WORKER ('+
                             ' Name,PassWd,account_limit,pkdeptid,ID,ShowAllTj) values ('+
                             ':P_Name,:P_PassWd,:P_account_limit,:P_pkdeptid,:P_ID,:ShowAllTj) ';
+        adotemp12.Parameters.ParamByName('P_ID').Value:=trim(uppercase(LabeledEdit1.Text)) ;
         adotemp12.Parameters.ParamByName('P_Name').Value:=trim(uppercase(LabeledEdit2.Text)) ;
         adotemp12.Parameters.ParamByName('P_PassWd').Value:='' ;
         adotemp12.Parameters.ParamByName('P_account_limit').Value:='' ;
-        adotemp12.Parameters.ParamByName('P_pkdeptid').Value:=ADOdeptlist.FieldByName('唯一编号').AsInteger; ;
-        adotemp12.Parameters.ParamByName('P_ID').Value:=trim(uppercase(LabeledEdit1.Text)) ;
+        adotemp12.Parameters.ParamByName('P_pkdeptid').Value:=dep_unid;
         adotemp12.Parameters.ParamByName('ShowAllTj').Value:=ComboBox1.Text ;
         adotemp12.ExecSQL;
         adotemp12.Free;
+
+        UpdateADOdoclist;
+        
         iid:=trim(LabeledEdit1.Text);
-        ADOdoclist.Close;
-        ADOdoclist.Open;
         ADOdoclist.Locate('用户代码',iid,[loCaseInsensitive]);
    end else
    begin
@@ -141,18 +148,17 @@ Begin
         adotemp11.Connection:=DM.ADOConnection1;
         adotemp11.Close;
         adotemp11.SQL.Clear;
-        adotemp11.SQL.Text:='update worker set id=:id,name=:name,ShowAllTj=:ShowAllTj '+
-                            'where unid=:unid';
+        adotemp11.SQL.Text:='update worker set id=:id,name=:name,ShowAllTj=:ShowAllTj where unid=:unid';
         try
-        adotemp11.Parameters.ParamByName('id').Value:=trim(uppercase(LabeledEdit1.Text));
-        adotemp11.Parameters.ParamByName('name').Value:=trim(uppercase(LabeledEdit2.Text));
-        adotemp11.Parameters.ParamByName('unid').Value:=ADOdoclist.FieldByName('唯一编号').AsInteger;
-        adotemp11.Parameters.ParamByName('ShowAllTj').Value:=ComboBox1.Text ;
-        adotemp11.ExecSQL;
+          adotemp11.Parameters.ParamByName('id').Value:=trim(uppercase(LabeledEdit1.Text));
+          adotemp11.Parameters.ParamByName('name').Value:=trim(uppercase(LabeledEdit2.Text));
+          adotemp11.Parameters.ParamByName('ShowAllTj').Value:=ComboBox1.Text ;
+          adotemp11.Parameters.ParamByName('unid').Value:=ADOdoclist.FieldByName('唯一编号').AsInteger;
+          adotemp11.ExecSQL;
         except
-        showmessage('已存在该用户代码或用户代码为空，请更换！');
-        adotemp11.Free;
-        exit;
+          MessageDlg('已存在该用户代码，请更换！',mtError,[MBOK],0);
+          adotemp11.Free;
+          exit;
         end;
         adotemp11.Free;
         ADOdoclist.Refresh;
@@ -161,80 +167,51 @@ end;
 
 procedure Tfrmdocset.docrefresh;
 begin
-        ifdocnewadd:=false;
-        if (ADOdoclist.Active) and (ADOdoclist.RecordCount>0) then
-        begin
-            LabeledEdit1.Text:=trim(ADOdoclist.FieldByName('用户代码').AsString);
-            LabeledEdit2.Text:=trim(ADOdoclist.FieldByName('用户名称').AsString);
-            ComboBox1.Text:=ADOdoclist.FieldByName('所有科室项目').AsString;
-        end else
-        begin
-            LabeledEdit1.Clear;
-            LabeledEdit2.Clear;
-            ComboBox1.Text:='';
-        end;
-end;
-
-procedure Tfrmdocset.DBGrid1CellClick(Column: TColumn);
-begin
-      docrefresh;
+  ifdocnewadd:=false;
+  
+  if (ADOdoclist.Active) and (ADOdoclist.RecordCount>0) then
+  begin
+      LabeledEdit1.Text:=trim(ADOdoclist.FieldByName('用户代码').AsString);
+      LabeledEdit2.Text:=trim(ADOdoclist.FieldByName('用户名称').AsString);
+      ComboBox1.Text:=ADOdoclist.FieldByName('所有科室项目').AsString;
+  end else
+  begin
+      LabeledEdit1.Clear;
+      LabeledEdit2.Clear;
+      ComboBox1.Text:='';
+  end;
 end;
 
 procedure Tfrmdocset.suiButton3Click(Sender: TObject);
 begin
-    if not ADOdoclist.Active then exit;
-    if ADOdoclist.RecordCount=0 then exit;
-    if application.MessageBox('你将删除选定的记录,继续吗?','系统提示', MB_OKCANCEL+MB_ICONWARNING)=1  then
-    begin
+  if not ADOdoclist.Active then exit;
+  if ADOdoclist.RecordCount<=0 then exit;
+  
+  if application.MessageBox('你将删除选定的记录,继续吗?','系统提示', MB_OKCANCEL+MB_ICONWARNING)=1 then
+  begin
     ADOdoclist.Delete;
     ADOdoclist.Refresh;
-    end;
+  end;
 end;
 
 procedure Tfrmdocset.FormCreate(Sender: TObject);
 begin
-  ADOdeptlist.Connection:=DM.ADOConnection1;
+  ifdocnewadd:=false;
+
   ADOdoclist.Connection:=DM.ADOConnection1;
 
   ChangeYouFormAllControlIme(Self);//设置中文输入法
 end;
 
-procedure Tfrmdocset.FormClose(Sender: TObject; var Action: TCloseAction);
-begin
-  action:=cafree;
-  if ffrmdocset=self then ffrmdocset:=nil;
-end;
-
 procedure Tfrmdocset.ADOdoclistAfterScroll(DataSet: TDataSet);
 begin
-      docrefresh;
-end;
-
-procedure Tfrmdocset.suiButton2KeyDown(Sender: TObject; var Key: Word;
-  Shift: TShiftState);
-begin
-if key=13 then suiButton2Click(nil);
+  docrefresh;
 end;
 
 procedure Tfrmdocset.FormShow(Sender: TObject);
 begin
-    ifdocnewadd:=false;
-    ADOdeptlist.Close;
-    ADOdeptlist.Open;
-    ActionList1.Actions[0].OnExecute:=suiButton1Click;
-    ActionList1.Actions[1].OnExecute:=suiButton2Click;
-    ActionList1.Actions[2].OnExecute:=suiButton3Click;
-    DBLookupComboBox1.KeyField:='科室名称';
-
-    if DBLookupComboBox1.Text='' then
-         SendKeyToControl(VK_DOWN,DBLookupComboBox1);
-
-    ADOdoclist.Close;
-    ADOdoclist.SQL.Clear;
-    ADOdoclist.SQL.Text:=sqlstr;
-    ADOdoclist.Parameters.ParamByName('P_pkdeptid').Value:=ADOdeptlist.fieldbyname('唯一编号').AsInteger;
-    ADOdoclist.Open;
-    docrefresh;
+  UpdateADOdoclist;
+  docrefresh;
 end;
 
 procedure Tfrmdocset.BitBtn1Click(Sender: TObject);
@@ -253,6 +230,39 @@ begin
   LYDataToExcel1.Execute;
   
   adotemp11.Free;
+end;
+
+procedure Tfrmdocset.ComboBox2DropDown(Sender: TObject);
+begin
+  LoadGroupName(TComboBox(Sender),'select CONCAT(''['',unid,'']'',Name) from CommCode WITH(NOLOCK) where TypeName=''部门'' order by id');
+end;
+
+procedure Tfrmdocset.UpdateADOdoclist;
+begin
+  ADOdoclist.Close;
+  ADOdoclist.SQL.Clear;
+  if trim(GetDepUnid)='' then
+  begin
+    ADOdoclist.SQL.Text:='SELECT id AS 用户代码,name AS 用户名称,ShowAllTj as 所有科室项目,unid AS 唯一编号 FROM worker order by id';
+  end else
+  begin
+    ADOdoclist.SQL.Text:='SELECT id AS 用户代码,name AS 用户名称,ShowAllTj as 所有科室项目,unid AS 唯一编号 FROM worker where pkdeptid='+trim(GetDepUnid)+' order by id';
+  end;
+  ADOdoclist.Open;
+end;
+
+procedure Tfrmdocset.ComboBox2Change(Sender: TObject);
+begin
+  UpdateADOdoclist;
+end;
+
+function Tfrmdocset.GetDepUnid: string;
+var
+  s2:string;
+begin
+  s2:=leftstr(ComboBox2.Text,pos(']',ComboBox2.Text));
+  Result:=StringReplace(s2,'[','',[rfReplaceAll, rfIgnoreCase]);
+  Result:=StringReplace(Result,']','',[rfReplaceAll, rfIgnoreCase]);
 end;
 
 initialization
